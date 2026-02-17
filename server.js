461015d2544437e24d628cfd8b9b415f0b375f06
Rodrigo <rodrigoss1053@gmail.com>
2026-02-17 18:19:19 -0300

    250

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/criar-pagamento', async (req, res) => {
         items: [{
           title: "Assinatura Plano Profissional - Salaodigital",
           quantity: 1,
-          unit_price: 29.90,
+          unit_price: 250.00,
           currency_id: 'BRL'
         }],
         metadata: { barber_id: barberId },
