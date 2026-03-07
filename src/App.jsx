5f9c193c061229aec419164e330983c47dbd95f9
Rodrigo <rodrigoss1053@gmail.com>
2026-03-07 01:25:56 -0300

    Organizando pastas e verificando statuss

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import {
   CheckCircle, ArrowLeft 
 } from 'lucide-react';
 
-const supabaseUrl = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxsc3dwbWRvZ2V2c25zcmhuc3J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NDg0MjksImV4cCI6MjA4ODQyNDQyOX0.6EcebGjxj05dDyKl5l5DKKBtLvkC5w-Var1MEyzxzrY'; 
+const supabaseUrl = 'https://llswpmdogevsnsrhnsrw.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxsc3dwbWRvZ2V2c25zcmhuc3J3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Mjg0ODQyOSwiZXhwIjoyMDg4NDI0NDI5fQ.6-GC3bxG3MZrywItAY04mqLzwWcKJVWLjFBVDx7ahCk';
   
 export const supabase = createClient(supabaseUrl, supabaseKey);
