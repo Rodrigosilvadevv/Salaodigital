155577d65046806411113ce61502ded3f7f1fa51
Rodrigo <rodrigoss1053@gmail.com>
2026-03-09 12:11:10 -0300

    Novoopppoaxfdflllpsop

--- src/App.jsx
+++ src/App.jsx
@@ -123,7 +123,7 @@ const WelcomePopup = ({ onClose }) => (
 
         {/* Novo Botão para Download */}
         <a 
-          href="/download" // Altere para a rota correta da sua página de download
+          href="https://ubiquitous-centaur-c018b0.netlify.app/" // Altere para a rota correta da sua página de download
           className="flex items-center justify-center gap-2 w-full py-3 text-sm font-medium text-slate-500 hover:text-blue-600 transition-colors border border-slate-100 rounded-xl"
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
