d6242e1e81bd774f2acdb54a6aac3ecec9cbcf13
Rodrigo <rodrigoss1053@gmail.com>
2026-06-10 15:37:49 -0300

    Fix: Reeesolvendo conflitos

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import {
 const APP_VERSION = 'v6.2';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
