81840fb7886c8f1fc02750d00c0236940d453fc9
Rodrigo <rodrigoss1053@gmail.com>
2026-08-14 19:15:40 -0300

    Minhas alterações protegendo o .env

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ import {
 
 const APP_VERSION = 'v6.3';
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 // Inicialização do cliente Supabase
 export const supabase = createClient(supabaseUrl, supabaseKey);
